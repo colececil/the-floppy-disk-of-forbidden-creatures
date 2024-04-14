8d506b33a8e4c196772a761051dab1f57bb69f2f
Cole Cecil <cole.cecil@gmail.com>
2024-04-14 16:25:41 -0500

    Figure out Bubble Tea text input component

--- internal/game/game.go
+++ internal/game/game.go
@@ -1,6 +1,7 @@
 package game
 
 import (
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/colececil/the-floppy-disk-of-forbidden-creatures/internal/messages"
 	"github.com/muesli/reflow/wordwrap"
@@ -13,18 +14,20 @@ type Game struct {
 	terminalHeight  int
 	previousText    string
 	activeText      string
+	input           textinput.Model
 }
 
 // NewGame creates a new Game.
 func NewGame() *Game {
 	return &Game{
 		messageProvider: messages.NewMessageProvider(),
+		input:           textinput.New(),
 	}
 }
 
 // Init performs initialization when the model is first created.
 func (g *Game) Init() tea.Cmd {
-	return g.updateGameState
+	return tea.Batch(g.updateGameState, textinput.Blink)
 }
 
 // Update is called when a message is received, and it returns an updated model and an optional command.
@@ -37,21 +40,25 @@ func (g *Game) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		if msg.Type == tea.KeyCtrlC {
 			return g, tea.Quit
 		}
-		return g, nil
-	default:
-		return g, g.updateGameState
 	}
+
+	var inputCmd tea.Cmd
+	g.input, inputCmd = g.input.Update(msg)
+	return g, tea.Batch(g.updateGameState, inputCmd)
 }
 
 // View renders the model as a string to be printed. This is called after every Update.
 func (g *Game) View() string {
 	return wordwrap.String(g.previousText, g.terminalWidth) + "\n\n" +
-		wordwrap.String(g.activeText, g.terminalWidth) + "\n\n"
+		wordwrap.String(g.activeText, g.terminalWidth) + "\n\n" +
+		g.input.View()
 }
 
+// updateGameState updates the game state.
 func (g *Game) updateGameState() tea.Msg {
 	if (g.previousText == "") && (g.activeText == "") {
-		g.activeText = g.messageProvider.GetMessage(messages.IntroMessage)
+		g.previousText = g.messageProvider.GetMessage(messages.IntroMessage)
+		g.activeText = g.messageProvider.GetMessage(messages.BeginRitualMessage)
 	}
 
 	return nil
